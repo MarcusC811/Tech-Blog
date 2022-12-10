0915e4d9a0d08d6d4e4f43de44108a7576a41513
OVOMARK <mcncstatefan@icloud.com>
2022-12-09 19:56:05 -0500

    updated server.js for connection launching and added user model for database logging

---
+++ models/Post.js
@@ -0,0 +1,13 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+Post.init(
+    {
+        title: {
+
+        },
+        comment: {
+
+        },
+    }
+)
